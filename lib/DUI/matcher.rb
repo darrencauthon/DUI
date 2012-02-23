cd7defb8e61df89dfe2a4350549234e32b065d1b
Darren Cauthon <darren@cauthon.com>
2012-02-22 21:16:13 -0600

    More passing specs.

--- lib/DUI/matcher.rb
+++ lib/DUI/matcher.rb
@@ -4,15 +4,26 @@ module DUI
     attr_accessor :current_data, :new_data
 
     def records_to_delete
+      @current_data.each do |c|
+        return [] if @new_data.select{|n| c.id == n.id}.count == 1
+      end
+      return @current_data if @new_data 
       []
     end
 
     def records_to_update
+      @current_data.each do |c|
+        return [c] if @new_data.select{|n| c.id == n.id}.count == 1
+      end
       []
     end
 
     def records_to_insert
-      @new_data
+      records = []
+      @new_data.each do |n|
+        records << n unless @current_data.select{|c| c.id == n.id}.count == 1
+      end
+      records
     end
   end
 end
--- spec/DUI/matcher_spec.rb
+++ spec/DUI/matcher_spec.rb
@@ -33,14 +33,127 @@ describe "Matcher" do
         @matcher.new_data = [TestProduct.new(1)]
       end
     
+      it "should have no records to delete" do
+        assert_equal 0, @matcher.records_to_delete.count
+      end
+
+      it "should have no records to update" do
+        assert_equal 0, @matcher.records_to_update.count
+      end
+
       it "should have one records to insert" do
         assert_equal 1, @matcher.records_to_insert.count
       end
 
-      it "should return the record in the new data" do
+      it "should return the new record as a record to insert" do
         @matcher.new_data.each do |x|
           assert_equal true, @matcher.records_to_insert.include?(x)
         end
+      end
+    end
+    
+  end
+
+  describe "when there is one existing record" do
+    before do
+      @matcher.current_data = [TestProduct.new(3)]
+    end
+  
+    describe "and it is given no new data" do
+      before do
+        @matcher.new_data = []
+      end
+
+      it "should have one records to delete" do
+        assert_equal 1, @matcher.records_to_delete.count
+      end
+
+      it "should return the current record as a record to delete" do
+        @matcher.current_data.each do |x|
+          assert_equal true, @matcher.records_to_delete.include?(x)
+        end
+      end
+
+      it "should have no records to update" do
+        assert_equal 0, @matcher.records_to_update.count
+      end
+
+      it "should have no records to insert" do
+        assert_equal 0, @matcher.records_to_insert.count
+      end
+    end
+
+    describe "and it is given the same record" do
+      before do
+        @matcher.new_data = [TestProduct.new(3)]
+      end
+    
+      it "should have no records to delete" do
+        assert_equal 0, @matcher.records_to_delete.count
+      end
+
+      it "should have one records to update" do
+        assert_equal 1, @matcher.records_to_update.count
+      end
+
+      it "should return the existing record as a record to update" do
+        assert_equal 1, @matcher.records_to_update.select{|x| x.id == 3}.count
+      end
+
+      it "should have no records to insert" do
+        assert_equal 0, @matcher.records_to_insert.count
+      end
+    end
+
+    describe "and it is given a different record" do
+      before do
+        @matcher.new_data = [TestProduct.new(5)]
+      end
+    
+      it "should have no records to delete" do
+        assert_equal 1, @matcher.records_to_delete.count
+      end
+
+      it "should return the existing record as a record to delete" do
+        assert_equal 1, @matcher.records_to_delete.select{|x| x.id == 3}.count
+      end
+
+      it "should have no records to update" do
+        assert_equal 0, @matcher.records_to_update.count
+      end
+
+      it "should have one record to insert" do
+        assert_equal 1, @matcher.records_to_insert.count
+      end
+
+      it "should return the new record as a record to insert" do
+        assert_equal 1, @matcher.records_to_insert.select{|x| x.id == 5}.count
+      end
+    end
+
+    describe "and it is given the existing record and the same record" do
+      before do
+        @matcher.new_data = [TestProduct.new(3), TestProduct.new(5)]
+      end
+    
+      it "should have no records to delete" do
+        assert_equal 0, @matcher.records_to_delete.count
+      end
+
+      it "should have one records to update" do
+        assert_equal 1, @matcher.records_to_update.count
+      end
+
+      it "should return the existing record as a record to update" do
+        assert_equal 1, @matcher.records_to_update.select{|x| x.id == 3}.count
+      end
+
+      it "should have one records to insert" do
+        assert_equal 1, @matcher.records_to_insert.count
+      end
+
+      it "should return the new records as a record to insert" do
+        assert_equal 1, @matcher.records_to_insert.select{|x| x.id == 5}.count  
       end
     end
     
