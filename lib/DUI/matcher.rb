3f4a759883b055f7a503b49d138c979e9dcea071
Darren Cauthon <darren@cauthon.com>
2012-02-23 12:57:06 -0600

    Refactor.

--- lib/DUI/matcher.rb
+++ lib/DUI/matcher.rb
@@ -12,27 +12,15 @@ module DUI
     private 
 
     def get_records_to_insert
-      records = []
-      @new_data.each do |n|
-        records << n unless @current_data.select{|c| c.id == n.id}.count == 1
-      end
-      records
+      return @new_data.select{|n| @current_data.select{|c| c.id == n.id}.count == 0 }
     end
 
     def get_records_to_delete
-      records = []
-      @current_data.each do |c|
-        records << c unless @new_data.select{|n| c.id == n.id}.count == 1
-      end
-      records
+      return @current_data.select{|c| @new_data.select{|n| c.id == n.id}.count == 0 }
     end
 
     def get_records_to_update
-      records = []
-      @current_data.each do |c|
-        records << c if @new_data.select{|n| c.id == n.id}.count == 1
-      end
-      records
+      @current_data.select{|c| @new_data.select{|n| c.id == n.id}.count == 1}
     end
   end
 end
