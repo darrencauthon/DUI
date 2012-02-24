6af128f65bc489e4a1e25390c2e0c071c36eeece
Darren Cauthon <darren@cauthon.com>
2012-02-23 20:10:22 -0600

    Refactor.

--- lib/DUI/matcher.rb
+++ lib/DUI/matcher.rb
@@ -7,25 +7,23 @@ module DUI
     end
 
     def execute(current_data, new_data)
-      @current_data = current_data
-      @new_data = new_data
-      Hashie::Mash.new(:records_to_delete => get_records_to_delete, 
-                       :records_to_update => get_records_to_update,
-                       :records_to_insert => get_records_to_insert)
+      Hashie::Mash.new(:records_to_delete => get_records_to_delete(current_data, new_data), 
+                       :records_to_update => get_records_to_update(current_data, new_data),
+                       :records_to_insert => get_records_to_insert(current_data, new_data))
     end
 
     private 
 
-    def get_records_to_insert
-      return @new_data.select {|n| @current_data.select {|c| @compare_method.call(c, n) }.count == 0 }
+    def get_records_to_insert(current_data, new_data)
+      return new_data.select {|n| current_data.select {|c| @compare_method.call(c, n) }.count == 0 }
     end
 
-    def get_records_to_delete
-      return @current_data.select {|c| @new_data.select {|n| @compare_method.call(c, n) }.count == 0 }
+    def get_records_to_delete(current_data, new_data)
+      return current_data.select {|c| new_data.select {|n| @compare_method.call(c, n) }.count == 0 }
     end
 
-    def get_records_to_update
-      @current_data.select {|c| @new_data.select {|n| @compare_method.call(c, n) }.count == 1}
+    def get_records_to_update(current_data, new_data)
+      current_data.select {|c| new_data.select {|n| @compare_method.call(c, n) }.count == 1}
     end
   end
 end
