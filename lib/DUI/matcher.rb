ec874a9155a2c9a9758ee7e85ab45d0d07a37c9a
Darren Cauthon <darren@cauthon.com>
2012-02-29 22:15:15 -0600

    Even more refactoring.

--- lib/DUI/matcher.rb
+++ lib/DUI/matcher.rb
@@ -7,29 +7,40 @@ module DUI
     end
 
     def get_results(current_data, new_data)
-      results = Hashie::Mash.new(:records_to_delete => [], :records_to_update => [])
+      results = get_the_results_of_the_delete_and_update_process(current_data, new_data) 
+      results.records_to_insert = get_records_to_insert(results.records_to_update.map{|u| u.current}, new_data)
+      results
+    end
+
+    private 
+
+    def get_the_results_of_the_delete_and_update_process(current_data, new_data)
+      results = an_object_with(:records_to_delete => [], :records_to_update => [])
       all_current_data_with_matches_in_new_data(current_data, new_data).each do |match| 
-        if match.no_match_found_in_new_data
+        if match.current_not_found_in_new
           results.records_to_delete << match.current
         else
           results.records_to_update << match 
         end
       end
-      results.records_to_insert = get_records_to_insert(results.records_to_update.map{|u| u.current}, new_data)
       results
     end
 
     def all_current_data_with_matches_in_new_data(current_data, new_data)
       current_data.map do |c| 
-        match = Hashie::Mash.new({:current => c})
+        match = an_object_with({:current => c})
         match.new = new_data.select {|n| @compare_method.call(c, n) }.first
-        match.no_match_found_in_new_data = match.new.nil?
+        match.current_not_found_in_new = match.new.nil?
         match
       end
     end
 
     def get_records_to_insert(current_data, new_data)
       new_data.select {|n| current_data.select {|c| @compare_method.call(c, n) }.count == 0 }
+    end
+
+    def an_object_with(hash)
+      Hashie::Mash.new(hash)
     end
   end
 end
